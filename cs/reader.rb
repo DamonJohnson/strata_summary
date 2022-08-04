111e9125997e75d9b4350ae111cdf142008ef3bd
DamonJohnson <85825667+DamonJohnson@users.noreply.github.com>
2022-08-04 16:35:45 +1000

    unit definition

--- cs/reader.rb
+++ cs/reader.rb
@@ -10,8 +10,27 @@ def get_num_entries(data)
 end
 
 # Unit Summary Table
-def get_origins(data)
-  data["GEOL_ORIG"].uniq.map { |origin| origin[0].upcase + origin[1..] }
-end
+# Hash to determine the order of units in the
+unit_order = {  
+  Topsoil: 0,
+  Fill: 1,
+  Colluvium: 2,
+  Alluvium: 3,
+  Estuarine: 3,
+  Residual: 4,
+  Weathered_Material: 5,
+  Rock: 6
+}
 
+
+
+def get_units(geoldata, gosadata)
+  origins = geoldata["GEOL_ORIG"].uniq.map { |origin| origin[0].upcase + origin[1..] }
+  units = Hash.new
+
+  if origins.include? 'Topsoil' && 'Fill' 
+    units["Unit 1"] = 'Topsoil/Fill'
+  end
+
+end
 
